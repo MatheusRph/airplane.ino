0c8063ac4740774a9aad11e2a4fbcaeb97303aba
MatheusRph <matheus.rodolpho16@gmail.com>
2023-11-08 16:29:32 -0300

    Corrected code

--- uno.ino
+++ uno.ino
@@ -7,11 +7,21 @@
 
 RF24 radio(12,11);  //Define os pinos do módulo
 
-const byte endereco[6] = {0xDE, 0xAD, 0xBE, 0xEF, 0x12, 0x34};  //Define o endereço do módulo NRF24
+const uint64_t endereco = 0xE8E8F0F0E1LL;     //Define o endereço do módulo NRF24
 
 //Definições dos joysticks
 int joyX = A0;      //seta a porta do joystick(eixo X)
 int joyY = A1;      // Seta a porta do joystick(eixo Y)
+
+// //Cria uma estrutura com bytes
+struct Dados_para_enviar {
+    byte ch1;
+    byte ch2;
+    byte ch3;
+};
+
+// //Cria uma variavel para dados_para_enviar
+dados_para_enviar dados_enviados;
 
 void setup() {
     radio.begin();  //Inicializa a comunicação wireless
@@ -19,6 +29,11 @@ void setup() {
     radio.setPALevel(RF24_PA_HIGH); //Define a potência do transmissor
     radio.setDataRate(RF24_2MBPS);  //Define a velocidade de transmissão do módulo
     radio.stopListening(); //Define o módulo transmissor para não receber dados
+
+    //Define os valores padrões dos canais
+    dados_enviados.ch1 = 127;
+    dados_enviados.ch2 = 127;
+    dados_enviados.ch3 = 127;
 }
 
 
@@ -28,7 +43,9 @@ void loop() {
     int joyXVal = analogRead(joyX); 
     int joyYVal = analogRead(joyY);
 
+    dados_enviados.ch1 = map(joyXVal, 0, 1023, 255, 0);
+    dados_enviados.ch2 = map(joyYVal, 0, 1023, 255, 0);
+
     //Escrevendo ou seja enviando os dados da leitura feitos acima do joystick
-    radio.write(&joyXVal, sizeof(joyXVal)); 
-    radio.write(&joyYVal, sizeof(joyYVal)); 
+    radio.write(&dados_enviados, sizeof(Dados_para_enviar));
 }
