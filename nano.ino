e681bbb51dab7d831859c2a5a9cb0aabcc0fc2ad
MatheusRph <matheus.rodolpho16@gmail.com>
2023-11-08 21:18:37 -0300

    Corrijed code

--- nano.ino
+++ nano.ino
@@ -1,21 +1,104 @@
 //Bibliotecas adicionadas
 #include <RF24.h>
+#include <SPI.h>
+#include <nRF24L01.h> 
 #include <Servo.h>
 
+//Definições wireless
+RF24 radio(12,11);  //Define os pinos do módulo
+
+const uint64_t endereco = 0xE8E8F0F0E1LL;     //Define o endereço do módulo NRF24
+
+//Definições do motor
 
 
 //Definições dos servos - Profundor, Flaps, Aileron, Lemo
-Servo FlapE;    //Define o nome do servo para FlapE
-Servo FlapD;    //Define o nome do servo para FlapD
-Servo Lemo;     //Define o nome do servo para Lemo
+Servo flapE;    //Define o nome do servo para FlapE
+Servo flapD;    //Define o nome do servo para FlapD
+servo profunder;    //Define o nome do servo para Profunder
+Servo lemo;     //Define o nome do servo para Lemo
+
+//Estrutura (função Dados recebidos)
+struct Dados_recebidos{
+    byte ch1;   //Flap Direita
+    byte ch2;   //Flap Esquerda
+    byte ch3;   //Profunder
+    byte ch4;   //Lemo
+    byte ch5;   //Acelerador
+}
+
+//Cria uma variavel para Dados_recebidos
+Dados_recebidos dados_recebidos;
+
+//Cria uma variável para armazenar os valores recebidos
+int flapD_value = 0;
+int lemo_value = 0;
+int flapE_value = 0;
+int prof_value = 0;
+int acel_value = 0;
+
+//Cria a função redefinir dados
+void redefinir_dados(){
+    //Redefini os dados
+    dados_recebidos.ch1 = 127;
+    dados_recebidos.ch2 = 127;
+    dados_recebidos.ch3 = 127;
+    dados_recebidos.ch4 = 127;
+    dados_recebidos.ch5 = 0;
+}
 
 void setup(){
+    flapE.attach(4);    // Pino do FlapE (Pode ser o 4 ou 5)
+    flapD.attach(5);    // Pino do FlapD (Pode ser o 4 ou 5)
+    lemo.attach(2);    // Pino do Lemo ()
+    prof.attach(3);      // Pino do Profunder
 
-    FlapE.attach(9);  // Pino do FlapE
-    FlapD.attach(10); // Pino do FlapD
-    Lemo.attach(11);  // Pino do Lemo
+
+    //Chama a função para redefinir os dados
+    redefinir_dados();
+
+    radio.begin();  //Inicializa a comunicação wireless
+    radio.setAutoAck(false);    //Define o endereço da comunicação do transmissor 
+    radio.setDataRate(RF24_2MBPS); //Define a velocidade de transmição  
+    radio.openReadingPipe(1,endereco);
+    radio.startListening();
+}
+
+//Variavel para armazenar tempo
+unsigned long lastRecvTime = 0;
+
+//Cria uma função que receberá os dados
+void receber_os_dados(){
+    //Inicia um loop se a conexão estiver ativa
+    while ( radio.available() ) {
+        radio.read(&dados_recebidos, sizeof(Dados_recebidos)); //Faz a leitura
+        lastRecvTime = millis(); //Transfere para milissegundos
+    }
 }
 
 void loop(){
+    //Chama a função que recebe os dados
+    receber_os_dados()
     
+    //Cria variavel que conta tempo atual em milissegundos desde o início do programa
+    unsigned long now = millis();
+    if ( now - lastRecvTime > 1000 ) {
+    //Sinal perdido?
+    receber_os_dados();
+  } 
+
+    //Faz a conversão dos valores do joystick de graus para microssegundos
+    flapD_value = map(dados_recebidos.ch1,0,255,1000,2000);
+    flapE_value = map(dados_recebidos.ch2,0,255,1000,2000);
+    prof_value = map(dados_recebidos.ch3,0,255,1000,2000);
+    lemo_value = map(dados_recebidos.ch4,0,255,1000,2000);
+    acel_value = map(dados_recebidos.ch5,0,255,1000,2000);
+
+
+
+    //Escreve (envia) os valores já convertidos em microssegundos para os servos
+    flapD.writeMicroseconds(flapD_value);  
+    flapE.writeMicroseconds(flapE_value);  
+    lemo.writeMicroseconds(lemo_value);  
+    prof.writeMicroseconds(prof_value);
 }
--- uno.ino
+++ uno.ino
@@ -10,22 +10,28 @@ RF24 radio(12,11);  //Define os pinos do módulo
 const uint64_t endereco = 0xE8E8F0F0E1LL;     //Define o endereço do módulo NRF24
 
 //Definições dos joysticks
-int joyX = A0;      //seta a porta do joystick(eixo X)
-int joyY = A1;      // Seta a porta do joystick(eixo Y)
+int joyX = A2;      //Seta a porta do joystick(eixo X)  Flaps
+int joyY = A1;      //Seta a porta do joystick(eixo Y) - Profunder
+int joyL = A0;      //Seta a porta do joystick - Lemo
+int poten = A4;     //Seta a porta do potenciomentro responsável pela aceleração
 
-// //Cria uma estrutura com bytes
+//Cria uma estrutura com bytes
 struct Dados_para_enviar {
-    byte ch1;
-    byte ch2;
-    byte ch3;
+    byte ch1;   //Flap Direita
+    byte ch2;   //Flap Esquerda
+    byte ch3;   //Profunder
+    byte ch4;   //Lemo
+    byte ch5;   //Acelerador
+    byte ch6;   //Botão
 };
 
-// //Cria uma variavel para dados_para_enviar
+//Cria uma variavel para dados_para_enviar
 Dados_para_enviar dados_enviados;
 
 void setup() {
     radio.begin();  //Inicializa a comunicação wireless
-    radio.openWritingPipe(endereco);    //Define o endereço da comunicação so receptor 
+    radio.setAutoAck(false)
+    radio.openWritingPipe(endereco);    //Define o endereço da comunicação do receptor 
     radio.setPALevel(RF24_PA_HIGH); //Define a potência do transmissor
     radio.setDataRate(RF24_2MBPS);  //Define a velocidade de transmissão do módulo
     radio.stopListening(); //Define o módulo transmissor para não receber dados
@@ -34,6 +40,8 @@ void setup() {
     dados_enviados.ch1 = 127;
     dados_enviados.ch2 = 127;
     dados_enviados.ch3 = 127;
+    dados_enviados.ch4 = 127;
+    dados_enviados.ch5 = 0;
 }
 
 
@@ -42,9 +50,16 @@ void loop() {
     //Define a leitura analógica dos joysticks
     int joyXVal = analogRead(joyX); 
     int joyYVal = analogRead(joyY);
+    int joyLVal = analogRead(joyL);
+   
+    //Faz a leitura do potenciomentro
+    int potenVal = analogRead(poten); 
 
     dados_enviados.ch1 = map(joyXVal, 0, 1023, 255, 0);
-    dados_enviados.ch2 = map(joyYVal, 0, 1023, 255, 0);
+    dados_enviados.ch2 = map(joyXVal, 0, 1023, 0, 255);
+    dados_enviados.ch3 = map(joyYVal, 0, 1023, 255, 0);
+    dados_enviados.ch4 = map(joyLVal, 0, 1023, 255, 0);
+    dados_enviados.ch5 = map(potenVal, 0, 1023, 255, 0);
 
     //Escrevendo ou seja enviando os dados da leitura feitos acima do joystick
     radio.write(&dados_enviados, sizeof(Dados_para_enviar));
